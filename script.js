ff2d066fbd562d245ea5e19db0fabc4cf8446985
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-05-23 19:40:36 +0200

    added sound

--- script.js
+++ script.js
@@ -6,4 +6,22 @@ function getRandomItemIndex(array) {
     return Math.floor(Math.random() * array.length);
 }
 
-let randomIndex = getRandomItemIndex(zombieImg);
+let randomIndex = getRandomItemIndex(items);
+items[randomIndex].append(zombieImg);
+setInterval(function () {
+    randomIndex = getRandomItemIndex(items);
+    items[randomIndex].append(zombieImg);
+}, 2000)
+
+const soundBtn = document.getElementById('sound-btn');
+const bu = document.getElementById('sound-bu');
+soundBtn.onclick = function () {
+    if(bu.currentTime) {
+        bu.pause()
+        bu.currentTime = 0;
+        soundBtn.innerHTML = 'SOUND ON'
+    } else {
+        bu.play()
+        soundBtn.innerHTML = 'SOUND OFF'
+    }
+}
