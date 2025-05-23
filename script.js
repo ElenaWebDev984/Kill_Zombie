5e13dfd4e2ce14e07a467eaad6ae894b7f385226
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-05-23 20:38:41 +0200

    test missCounter

--- script.js
+++ script.js
@@ -1,52 +1,63 @@
-const items = document.querySelectorAll(".item")
-const zombieImg = document.createElement("img")
-zombieImg.src = "images/zombie.png"
-const missCounter = document.getElementById("miss-counter")
-let hit = false
+const items = document.querySelectorAll(".item");
+const zombieImg = document.createElement("img");
+zombieImg.src = "images/zombie.png";
+const missCounter = document.getElementById("miss-counter");
+let hit = false;
+let zombieVisible = true; // Добавляем флаг видимости зомби
 
 function getRandomItemIndex(array) {
-    return Math.floor(Math.random() * array.length)
+    return Math.floor(Math.random() * array.length);
 }
 
-let randomIndex = getRandomItemIndex(items)
-items[randomIndex].append(zombieImg)
+let randomIndex = getRandomItemIndex(items);
+items[randomIndex].append(zombieImg);
 
 setInterval(function () {
-    if(hit === true){
-        hit = false
-    } else {
-        missCounter.innerHTML++
+    // Если зомби был убит (hit = true), сбрасываем флаг и скрываем зомби
+    if(hit === true) {
+        hit = false;
+        zombieVisible = false;
+        zombieImg.remove();
+        items[randomIndex].append(hitImg);
     }
-    hitImg.remove()
-    randomIndex = getRandomItemIndex(items)
-    items[randomIndex].append(zombieImg)
-}, 2000)
+    // Если зомби не был убит и он виден - увеличиваем счетчик промахов
+    else if(zombieVisible) {
+        missCounter.innerHTML++;
+        zombieImg.remove();
+    }
 
+    // Показываем нового зомби
+    randomIndex = getRandomItemIndex(items);
+    items[randomIndex].append(zombieImg);
+    zombieVisible = true; // Устанавливаем флаг, что зомби виден
+}, 2000);
 
-const soundBtn = document.getElementById("sound-btn")
-const bu = document.getElementById("sound-bu")
+// Остальной код остается без изменений
+const soundBtn = document.getElementById("sound-btn");
+const bu = document.getElementById("sound-bu");
 
-soundBtn.onclick = function (params) {
+soundBtn.onclick = function () {
     if (bu.currentTime !== 0) {
-        bu.pause()
-        bu.currentTime = 0
-        soundBtn.innerHTML = "SOUND ON"
+        bu.pause();
+        bu.currentTime = 0;
+        soundBtn.innerHTML = "SOUND ON";
     } else {
-        bu.play()
-        soundBtn.innerHTML = "SOUND OFF"
+        bu.play();
+        soundBtn.innerHTML = "SOUND OFF";
     }
 }
 
-const hitImg = document.createElement("img")
-hitImg.src = "images/blood.png"
-const hitCounter = document.getElementById("hit-counter")
-const shot = document.getElementById("sound-shot")
+const hitImg = document.createElement("img");
+hitImg.src = "images/blood.png";
+const hitCounter = document.getElementById("hit-counter");
+const shot = document.getElementById("sound-shot");
 
 zombieImg.onclick = function () {
-    hit = true
+    hit = true;
     hitCounter.innerHTML++;
-    shot.currentTime = 0
-    shot.play()
-    zombieImg.remove()
-    items[randomIndex].append(hitImg)
+    shot.currentTime = 0;
+    shot.play();
+    zombieVisible = false; // Зомби убит, больше не виден
+    zombieImg.remove();
+    items[randomIndex].append(hitImg);
 }
