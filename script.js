59e75ec66c5454448eb2b68a803b76f1080b52af
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-05-23 21:55:38 +0200

    refactoring

--- script.js
+++ script.js
@@ -1,63 +1,80 @@
-const items = document.querySelectorAll(".item");
-const zombieImg = document.createElement("img");
-zombieImg.src = "images/zombie.png";
-const missCounter = document.getElementById("miss-counter");
-let hit = false;
-let zombieVisible = true; // Добавляем флаг видимости зомби
-
 function getRandomItemIndex(array) {
-    return Math.floor(Math.random() * array.length);
+    return Math.floor(Math.random() * array.length)
 }
 
-let randomIndex = getRandomItemIndex(items);
-items[randomIndex].append(zombieImg);
+const items = document.querySelectorAll(".item")
+const zombieImg = document.createElement("img")
+zombieImg.src = "images/zombie.png"
+const missCounter = document.getElementById("miss-counter")
+const soundBtn = document.getElementById("sound-btn")
+const bu = document.getElementById("sound-bu")
+const hitImg = document.createElement("img")
+hitImg.src = "images/blood.png"
+const hitCounter = document.getElementById("hit-counter")
+const shot = document.getElementById("sound-shot")
+const startBtn = document.getElementById("start-btn")
 
-setInterval(function () {
-    // Если зомби был убит (hit = true), сбрасываем флаг и скрываем зомби
-    if(hit === true) {
-        hit = false;
-        zombieVisible = false;
-        zombieImg.remove();
-        items[randomIndex].append(hitImg);
-    }
-    // Если зомби не был убит и он виден - увеличиваем счетчик промахов
-    else if(zombieVisible) {
-        missCounter.innerHTML++;
-        zombieImg.remove();
-    }
+let hit = false
+let start = false
+let randomIndex;
+let interval;
 
-    // Показываем нового зомби
-    randomIndex = getRandomItemIndex(items);
-    items[randomIndex].append(zombieImg);
-    zombieVisible = true; // Устанавливаем флаг, что зомби виден
-}, 2000);
 
-// Остальной код остается без изменений
-const soundBtn = document.getElementById("sound-btn");
-const bu = document.getElementById("sound-bu");
+// TODO перемещение зомби
+function startGame() {
 
-soundBtn.onclick = function () {
+    interval = setInterval(function () {
+        if (hit === true) {
+            hit = false
+        } else {
+            missCounter.innerHTML++
+        }
+        hitImg.remove()
+        randomIndex = getRandomItemIndex(items)
+        items[randomIndex].append(zombieImg)
+    }, 2000)
+}
+
+
+// TODO включение/выключение фонового звука
+soundBtn.onclick = function (params) {
     if (bu.currentTime !== 0) {
-        bu.pause();
-        bu.currentTime = 0;
-        soundBtn.innerHTML = "SOUND ON";
+        bu.pause()
+        bu.currentTime = 0
+        soundBtn.innerHTML = "SOUND ON"
     } else {
-        bu.play();
-        soundBtn.innerHTML = "SOUND OFF";
+        bu.play()
+        soundBtn.innerHTML = "SOUND OFF"
     }
 }
 
-const hitImg = document.createElement("img");
-hitImg.src = "images/blood.png";
-const hitCounter = document.getElementById("hit-counter");
-const shot = document.getElementById("sound-shot");
 
-zombieImg.onclick = function () {
-    hit = true;
-    hitCounter.innerHTML++;
-    shot.currentTime = 0;
-    shot.play();
-    zombieVisible = false; // Зомби убит, больше не виден
-    zombieImg.remove();
-    items[randomIndex].append(hitImg);
+// TODO включение/выключение
+startBtn.onclick = function () {
+    if (start === false) {
+        start = true
+        randomIndex = getRandomItemIndex(items)
+        // TODO первичное размещение зомби
+        items[randomIndex].append(zombieImg)
+        // TODO попадание в зомби
+        zombieImg.onclick = function () {
+            hit = true
+            hitCounter.innerHTML++;
+            shot.currentTime = 0
+            shot.play()
+            zombieImg.remove()
+            items[randomIndex].append(hitImg)
+        }
+        startGame()
+        startBtn.innerHTML = "STOP"
+    } else {
+        start = false
+        zombieImg.onclick = null
+        zombieImg.remove()
+        hitImg.remove()
+        missCounter.innerHTML = 0
+        hitCounter.innerHTML = 0
+        clearInterval(interval)
+        startBtn.innerHTML = "START"
+    }
 }
